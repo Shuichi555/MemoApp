456e964c9585a01791d35e7ef84d45b6ddb3b6df
Shuichi Chishima <chishimashuichi@ShuichinoMacBook-ea.local>
2018-04-18 19:14:48 +0900

    Start coding react-native 2018/04/18

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! Hi!!</Text>
+        <View style={styles.appbar}>
+          <View>
+            <Text>MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text styel={styles.memoDate}>2018/04/18</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text styel={styles.memoDate}>2018/04/18</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text styel={styles.memoDate}>2018/04/18</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text styel={styles.memoDate}>2018/04/18</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text styel={styles.memoDate}>2018/04/18</Text>
+          </View>
+
+        </View>
+
+        <View>
+          <Text>+</Text>
+        </View>
       </View>
     );
   }
