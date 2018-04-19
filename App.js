7e5a5e723e01ebde7c41765270ca02bcaee1b4e0
Shuichi Chishima <chishimashuichi@ShuichinoMacBook-ea.local>
2018-04-19 11:42:35 +0900

    Create memoList page

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <View style={styles.appbar}>
           <View>
-            <Text>MEMOT</Text>
+            <Text style={styles.appbarTitle}>MEMOT</Text>
           </View>
         </View>
 
@@ -16,33 +16,33 @@ export default class App extends React.Component {
 
           <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text styel={styles.memoDate}>2018/04/18</Text>
+            <Text style={styles.memoDate}>2018/04/18</Text>
           </View>
 
           <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text styel={styles.memoDate}>2018/04/18</Text>
+            <Text style={styles.memoDate}>2018/04/18</Text>
           </View>
 
           <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text styel={styles.memoDate}>2018/04/18</Text>
+            <Text style={styles.memoDate}>2018/04/18</Text>
           </View>
 
           <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text styel={styles.memoDate}>2018/04/18</Text>
+            <Text style={styles.memoDate}>2018/04/18</Text>
           </View>
 
           <View style={styles.memoListItem}>
             <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text styel={styles.memoDate}>2018/04/18</Text>
+            <Text style={styles.memoDate}>2018/04/18</Text>
           </View>
 
         </View>
 
-        <View>
-          <Text>+</Text>
+        <View style={styles.memoAddButton}>
+          <Text style={styles.memoAddButtonTitle}>+</Text>
         </View>
       </View>
     );
@@ -52,8 +52,73 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    // タイトル分だけスペース確保する
+    paddingTop: 78,
+  },
+  memoList: {
+    width: '100%',
+    // flexは画面全てをこの要素で覆う、という意味？
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+    // 階層を一段上に持っていくため？↓
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
+    // backgroundColor: '#ddd',
+    // width: 20,
+    // height: 20,
   },
 });
