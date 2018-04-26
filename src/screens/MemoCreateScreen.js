40f3195663fdee6177022864828724e9aee52eb0
Shuichi Chishima <chishimashuichi@ShuichinoMacBook-ea.local>
2018-04-26 19:07:46 +0900

    Get memoList from firestore

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -8,16 +8,24 @@ class MemoCreateScreen extends React.Component {
     body: '',
   }
   handlePress() {
-    const { params } = this.props.navigation.state;
+//  const { params } = this.props.navigation.state;
 
     const db = firebase.firestore();
     const settings = {timestampsInSnapshots: true};
     db.settings(settings);
 
+    const timestamp = snapshot.get('created_at');
+    const date = timestamp.toDate();
+
+    const { currentUser } = firebase.auth();
+
     // 変数を代入するため　"``"　を使用
-    db.collection(`users/${params.currentUser.uid}/memos`).add({
+//    db.collection(`users/${params.currentUser.uid}/memos`).add({
+      db.collection(`users/${currentUser.uid}/memos`).add({
       body: this.state.body,
-      createdOn: new Date(),
+
+//    createdOn: new Date(),
+      createdOn: date,
     })
         .then((docRef) => {
           console.log(docRef.id)
